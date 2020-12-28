37a111d933e860b4c96f20f736f95a3704868a70
Marcin Wołkowicz <mwolkowicz17@gmail.com>
2020-12-28 11:48:24 +0100

    Ui tests added

---
+++ Maratony.Ui.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Maratony.Ui.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Maratony.Ui.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("114110df-78cd-441a-8ba3-2f57e696d1ed")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Maratony.Ui.Test/ViewModelTest.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+using Maratony.UI.ViewModel;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Maratony.Ui.Test
+{
+    [TestClass]
+    public class ViewModelTest
+    {
+        [TestMethod]
+        public void TestNowyViewModel_OdswiezZawody()
+        {
+            var vm = new MaintenanceFormViewModel(true); // konstruktor dla UT
+            Assert.IsTrue(vm.Zawody.Count() > 0);
+        }
+
+        [TestMethod]
+        public void TestNowyViewModel_OdswiezBiegaczy()
+        {
+            var vm = new MaintenanceFormViewModel(true); // konstruktor dla UT
+            var wywolaneOdswiezBiegaczy = false;
+
+            vm.PropertyChanged += (s, e) =>
+            {
+                if (e.PropertyName == "Biegacze")
+                {
+                    wywolaneOdswiezBiegaczy = true;
+                }
+            };
+
+            vm.WybraneZawody = vm.Zawody.Skip(1).First();
+            Assert.IsTrue(wywolaneOdswiezBiegaczy);
+            Assert.IsTrue(vm.Biegacze.Count() > 0);
+        }
+
+    }
+}
